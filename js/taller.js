119dd53073e8ca5be648d4a6625ca3837d6e7082
VanePuentes <puentesvanessa00@gmail.com>
2024-05-09 08:21:18 -0500

    Proyecto Semana 3

---
+++ js/taller.js
@@ -0,0 +1,129 @@
+//  Punto numero uno
+let puntoUno = ["bread", "milk", "cheese", "hummus", "noodles"];
+for (let index = 0; index < puntoUno.length; index++) {
+    console.log(puntoUno[index]);
+    
+}
+
+//Punto numero dos
+let diasSemana=[]
+diasSemana.push ["lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"];
+
+
+// punto numero tres
+let personas = [
+        {
+            nombre: "Vanessa",
+            apellido: "Puentes",
+            telefono: "123456789",
+            edad: 23
+        },
+        {
+            nombre: "keila",
+            apellido: "Varela",
+            telefono: "987654321",
+            edad: 21
+        },
+        {
+            nombre: "Sharol",
+            apellido: "Gantiva",
+            telefono: "555123456",
+            edad: 28
+        },
+        {
+            nombre: "Laura",
+            apellido: "Pastra",
+            telefono: "789456123",
+            edad: 42
+        },
+        {
+            nombre: "Javier",
+            apellido: "Ramos",
+            telefono: "321654987",
+            edad: 18
+        }
+    ];
+
+
+    // punto numero cuatro
+    let estudiantes = [
+        {
+            nombre: "leonardo",
+            apellido: "Contreras",
+            telefono: "987654321",
+            edad: 30,
+            materias: ["Estadistica", "Matematicas", "Econometria"],
+            hobbiesFavoritos: ["Futbol", "Caminar", "Bailar"]
+                },
+                
+        {
+            nombre: "Vanessa",
+            apellido: "Puentes",
+            telefono: "987654321",
+            edad: 24,
+            materias: ["Inversiones", "Microeconomia", "Macroeconomia"],
+            hobbiesFavoritos: ["Maquillar", "Dibujar", "Videojuegos"]
+                },
+
+        {
+            nombre: "Sofia",
+            apellido: "Mendez",
+            telefono: "675654321",
+            edad: 21,
+            materias: ["Programación", "contabilidad", "estadistica"],
+            hobbiesFavoritos: ["Deporte", "Patinar", "Bailar"]
+                },
+
+        {
+            nombre: "Juan",
+            apellido: "Ortiz",
+            telefono: "678345690",
+            edad: 25,
+            materias: ["Programación", "algebra", "Historia"],
+            hobbiesFavoritos: ["Futbol", "Pintar", "Natacion"]
+                },
+                
+        {
+            nombre: "Maríana",
+            apellido: "Gomez",
+            telefono: "346892465",
+            edad: 26,
+            materias: ["Programación", "Matemáticas", "Historia"],
+            hobbiesFavoritos: ["videojuegos", "Caminar", "Bailar"]
+            },
+    ];
+        console.log(estudiantes)
+        console.log(materias)
+        console.log(hobbiesFavoritos)
+        
+    // punto numero cinco
+
+    let numerosYabecedario = [1, 2, 3, ["a", "b", "c"]];
+
+
+    // punto numero seis
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
